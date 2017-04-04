fa8a62e801dd5ac9b19a9c42c41932d3d431ae84
Xuefei Ren <gensmusic@163.com>
2017-04-04 18:35:45 +0800

    add url to a list

--- scripts/content.js
+++ scripts/content.js
@@ -6,18 +6,42 @@ function isDownload(url) {
     return false;
 }
 
+var CHOSEN_LIST = [];
+
 function modifyAllDownloadLinks() {
     $('a').each(function() {
         var that = $(this);
         var url = that.attr('href');
         if (isDownload(url)) {
             console.log(url);
-            var btn = $("<input type='button' id='testBtn' value='test'>")
+            var btn = $("<input type='button' id='testBtn' value='+'>")
             btn.bind('click', function(url) {
                 return function() {
-                    alert(url)
+                    console.log(url)
+                    if (CHOSEN_LIST.indexOf(url) === -1) {
+                        console.log(`add url to list: ${url}`)
+                        CHOSEN_LIST.push(url);
+                    } else {
+                        console.log(`already has this url: ${url}`)
+                    }
+                    console.log(CHOSEN_LIST)
                 }
             }(url))
+            btn.css({
+                'backgroud-color': '#F44336',
+                width: '25px',
+                height: '25px',
+                'border-radius': '100%',
+                background: '#F44336',
+                border: 'none',
+                outline: 'none',
+                color: '#FFF',
+                'font-size': '23px',
+                // 'box-shadow': '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
+                transition: '.3s',
+                position: 'relative',
+                'z-index': 3000
+            })
             that.append(btn)
         }
     });
@@ -25,10 +49,11 @@ function modifyAllDownloadLinks() {
 
 chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
     console.log("something happening from the extension");
-    var data = request.data || {};
+    // var data = request.data || {};
+    var data = CHOSEN_LIST;
     console.log(`data is ${JSON.stringify(data)}`)
 
-    sendResponse({data: data, success: true});
+    sendResponse({data: CHOSEN_LIST, success: true});
 });
 
 $(document).ready(function(){
