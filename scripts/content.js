8a143b9454b94805b275dc695c2f22d34b40ce60
Xuefei Ren <gensmusic@163.com>
2017-04-08 09:14:06 +0800

    add button

--- scripts/content.js
+++ scripts/content.js
@@ -1,12 +1,12 @@
 function isDownload(url) {
-    var reg = /thunder:\/\/.*/
+    var reg = /thunder:\/\/.*|magnet:?.*/
     if (url && reg.test(url)) {
         return true;
     }
     return false;
 }
 
-var CHOSEN_LIST = [];
+let CHOSEN_LIST = new Set();
 
 function insertScreen() {
     let docWidth = $(document).width();
@@ -23,40 +23,63 @@ function insertScreen() {
         z-index: 3000;
     "></div>`)
     $('body').append(d);
+
+    // add css
+    $(`<style>
+    .copyLinksaddButton {
+        position: absolute;
+        background-color: #F44336;
+        width: 30px;
+        height: 30px;
+        border-radius: 100%;
+        background: #F44336;
+        border: none;
+        outline: none;
+        color: #FFF;
+        font-size: 20px;
+        box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+        transition: .3s;
+        -webkit-tap-highlight-color: rgba(0,0,0,0);
+    }
+    .copyLinksaddButton:focus {
+        transform:scale(1.1);
+        transform:rotate(45deg);
+        -ms-transform: rotate(45deg);
+        -webkit-transform: rotate(45deg);
+    }
+    </style>`).appendTo('head')
+}
+
+function newButton(top, left, url) {
+    let btn = $(`<buntton class="copyLinksaddButton">+</>`);
+    btn.css("top", `${top}px`);
+    btn.css("left", `${left}px`);
+
+    btn.bind('click', function() {
+        if (btn.text() == '+') {
+            CHOSEN_LIST.add(url);
+            btn.text('-')
+        } else {
+            CHOSEN_LIST.delete(url);
+            btn.text('+')
+        }
+        console.log(CHOSEN_LIST)
+    });
+    return btn;
 }
 
 function modifyAllDownloadLinks() {
     let screen = $('.screen');
     let screenTop = screen.offset().top;
     let screenLeft = screen.offset().left;
+
     $('a').each(function() {
         let that = $(this);
         let url = that.attr('href');
         if (isDownload(url)) {
-            let btn = $(`<i style="
-                position: absolute;
-                font-size: 18px;
-                color: '#FFF';
-                border: none;
-                background: #F44336;
-                display: block;
-                width: 20px;
-                height: 20px;
-                top: ${that.offset().top - screenTop}px;
-                left: ${that.offset().left - screenLeft}px;
-                border-radius: 50%;
-                z-index: 3000;
-            ">+</i>`)
-            btn.bind('click', function() {
-                if (CHOSEN_LIST.indexOf(url) === -1) {
-                    console.log(`add url to list: ${url}`)
-                    CHOSEN_LIST.push(url);
-                } else {
-                    console.log(`already has this url: ${url}`)
-                }
-                console.log(CHOSEN_LIST)
-            });
-            screen.append(btn)
+            let top = that.offset().top - screenTop;
+            let left = that.offset().left - screenLeft;
+            screen.append(newButton(top, left, url))
         }
     });
 }
@@ -66,12 +89,12 @@ chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
     // var data = request.data || {};
     var data = CHOSEN_LIST;
     console.log(`data is ${JSON.stringify(data)}`)
-
+    insertScreen();
+    modifyAllDownloadLinks();
     sendResponse({data: CHOSEN_LIST, success: true});
 });
 
 $(document).ready(function(){
     console.log(`All is ready!!!!!!!!!!!!!!!!!!!!!`)
-    insertScreen();
-    modifyAllDownloadLinks();
+
 });
